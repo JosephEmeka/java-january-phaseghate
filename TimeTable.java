81d0c66bd43532c07116277e06d7b457358866a1
JosephEmeka <josephemekaonah@gmail.com>
2024-01-19 09:36:25 +0100

    TimeTable.java

---
+++ TimeTable.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TimeTable{
+
+
+	public static void main (String[] args){
+
+
+		int result = 0;
+		
+		int number = 1;
+		
+		for (int count = 1; count < 13; count++){
+
+				for (int counter = 1; counter < 13; counter ++){
+
+					System.out.print(counter + "*" + count + "=" +  count * counter + "\t\t");
+	
+				
+					}
+
+			System.out.println();
+
+			
+
+
+		}
+
+
+	}
+
+}
