f15aed9d4a27d46fb32d385aa966476cc2e6335a
JosephEmeka <josephemekaonah@gmail.com>
2024-01-19 09:25:29 +0100

    NaturalNumber.java

---
+++ NaturalNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class NaturalNumber {
+
+
+	public static void main (String[] args){
+
+		int sum = 0;
+
+			for (int number = 0 ; number < 11; number++) {
+
+				sum += number;
+			      }
+			
+		System.out.println(sum);
+	
+
+		}
+
+
+}
